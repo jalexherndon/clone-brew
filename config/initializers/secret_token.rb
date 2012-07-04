627f2489f81fb50ad601e6f053933d6091a6a35d
Jacob Burton <jburton@rallydev.com>
2012-07-04 10:26:52 -0600

    Creating the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloneBrew::Application.config.secret_token = '7996db7a97601b40d390290b1a040103340b636e46b2629fa44d4a51c5abba0c2f431d5cc15bf573a9af21c51b1306018597a768e7b67dd9677f77a6306cac71'
