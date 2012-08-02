7ab88c46025cb7d01addd2b4e4f4808596e95dcd
alexherndon@gmail.com <alexherndon@gmail.com>
2012-08-01 19:24:32 -0600

    Setting up the viewport

---
+++ app/assets/javascripts/ui/ViewPort.js
@@ -0,0 +1,12 @@
+define("Brew/ui/ViewPort", [
+	"dojo/_base/declare",
+	"dijit/layout/BorderContainer"
+], function(declare, borderContainer){
+	
+	return declare("Brew.ui.ViewPort", borderContainer, {
+		
+		constructor: function(config) {
+			
+		}
+	});
+});
