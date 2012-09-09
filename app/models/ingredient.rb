8c391e707c4246073ae81efc1881d3e883bc4381
Jacob Burton <burtonjc35@gmail.com>
2012-09-09 01:08:40 -0600

    Fixing an issue with ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,13 +1,13 @@
 class Ingredient < ActiveRecord::Base
 
-  belongs_to  :category, :class_name => 'IngredientCategory'
+  belongs_to  :ingredient_category
   has_many    :ingredient_details
   has_many    :recipes, :through => :ingredient_details
 
   #has_and_belongs_to_many :recipes, :join_table => "recipes_ingredients"
 
   accepts_nested_attributes_for :ingredient_details
-  attr_accessible :description, :name, :category_id, :ingredient_detail_ids
+  attr_accessible :description, :name, :ingredient_category_id, :ingredient_detail_ids
   validates :name, :presence => true
-  validates :category_id, :presence => true
+  validates :ingredient_category_id, :presence => true
 end
