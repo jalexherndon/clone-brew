e6c0c74c5fb3de4e42babfb7465435f02219f80a
Jacob Burton <burtonjc35@gmail.com>
2012-12-06 14:31:57 -0700

    Setting production host name in devise config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,6 @@ CloneBrew::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Default URL options
-  config.action_mailer.default_url_options = { :host => 'clonebrews' }
+  config.action_mailer.default_url_options = { :host => 'clonebrews.herokuapp' }
 
 end
