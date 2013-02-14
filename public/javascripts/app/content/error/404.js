53f6f576dd8e7f8ddda7814a806736fb5ad09a2c
Jacob Burton <burtonjc35@gmail.com>
2013-02-13 23:17:37 -0700

    Removing generated js files from git

--- public/javascripts/app/content/error/404.js
+++
@@ -1,7 +0,0 @@
-(function() {
-
-  define('Brew/content/error/404', ['dojo/_base/declare', 'Brew/content/_Page'], function(declare, _Page) {
-    return declare('Brew.content.error.404', _Page, {});
-  });
-
-}).call(this);
