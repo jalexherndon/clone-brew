67428269f33481b929060defc79bf98a012db06d
Jacob Burton <burtonjc35@gmail.com>
2013-03-05 00:24:11 -0700

    adding a couple tests around mash_steps

--- spec/models/mash_step_spec.rb
+++ spec/models/mash_step_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
-class MashStepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe MashStep do
+  it "should not allow step_types outside of MashStep::STEP_TYPES" do
+    expect {
+      MashStepBuilder.basic_mash_step
+                     .with_step_type(MashStep::STEP_TYPES.length)
+                     .build
+                     .save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "should allow all step_types within MashStep::STEP_TYPES" do
+    for step_type in MashStep::STEP_TYPES
+      MashStepBuilder.basic_mash_step
+                     .with_step_type(step_type)
+                     .build
+                     .save!
+    end
+  end
 end
